c8fad4f9ba8588be5e87ecc25b247f5a99db75ed
nneleri638-del <nneleri638@gmail.com>
2025-10-01 09:59:21 +0630

    Testing

---
+++ SDLCEclipse/src/jhgoeu5l/irjkgteijp.java
@@ -0,0 +1,11 @@
+package jhgoeu5l;
+
+public class irjkgteijp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO World Testingg!");
+
+	}
+
+}
